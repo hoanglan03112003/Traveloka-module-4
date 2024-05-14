a1e8a0080fbddf4497ecdd5982864ea0a474c790
NguyenLannnFE <nguyenhoanglan0311@gmail.com>
2024-05-14 16:00:00 +0700

    message

--- src/Component/traveloke-rentcar/mainRentcarPopularCar.jsx
+++ src/Component/traveloke-rentcar/mainRentcarPopularCar.jsx
@@ -10,7 +10,7 @@ function RentcarPopularCar() {
         <div className="flex gap-2">
           <div className="relative cursor-pointer overflow-hidden rounded-lg border border-black m-1">
             <img
-              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110 hover:scale-110"
+              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110"
               src="https://ik.imagekit.io/tvlk/image/imageResource/2020/04/17/1587126697477-0ecd6745265eeb727821f2742563a5c7.jpeg?tr=q-75,w-295"
               alt=""
             />
@@ -20,7 +20,7 @@ function RentcarPopularCar() {
           </div>
           <div className="relative cursor-pointer overflow-hidden rounded-lg border border-black m-1">
             <img
-              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110 hover:scale-110"
+              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110"
               src="https://ik.imagekit.io/tvlk/image/imageResource/2020/04/17/1587126698279-895de43f4361a12ded2f3638f122fab4.jpeg?tr=q-75,w-295"
               alt=""
             />
@@ -30,7 +30,7 @@ function RentcarPopularCar() {
           </div>
           <div className="relative cursor-pointer overflow-hidden rounded-lg border border-black m-1">
             <img
-              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110 hover:scale-110"
+              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110"
               src="https://ik.imagekit.io/tvlk/image/imageResource/2020/04/17/1587126702222-abf2fa9d0f635f4cb7f75e7ce13cdd86.png?tr=q-75,w-295"
               alt=""
             />
@@ -42,7 +42,7 @@ function RentcarPopularCar() {
         <div className="flex gap-2">    
           <div className="relative cursor-pointer overflow-hidden rounded-lg border border-black m-1">
             <img
-              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110 hover:scale-110"
+              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110"
               src="https://ik.imagekit.io/tvlk/image/imageResource/2020/04/17/1587126705804-28809534f3a73a8f493ba633008fd080.jpeg?tr=q-75,w-295"
               alt=""
             />
@@ -52,7 +52,7 @@ function RentcarPopularCar() {
           </div>
           <div className="relative cursor-pointer overflow-hidden rounded-lg border border-black m-1">
             <img
-              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110 hover:scale-110"
+              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110"
               src="https://ik.imagekit.io/tvlk/image/imageResource/2020/04/17/1587126708321-2dfade8f99bebf40286c4a17880cce82.jpeg?tr=q-75,w-295"
               alt=""
             />
@@ -62,7 +62,7 @@ function RentcarPopularCar() {
           </div>
           <div className="relative cursor-pointer overflow-hidden rounded-lg border border-black m-1">
             <img
-              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110 hover:scale-110"
+              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110"
               src="https://ik.imagekit.io/tvlk/image/imageResource/2020/04/17/1587126711223-f007a3a9157d16ab62c5b3172e682f9f.jpeg?tr=q-75,w-295"
               alt=""
             />
@@ -74,7 +74,7 @@ function RentcarPopularCar() {
         <div className="flex gap-2">
           <div className="relative cursor-pointer overflow-hidden rounded-lg border border-black m-1">
             <img
-              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110 hover:scale-110"
+              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110"
               src="https://ik.imagekit.io/tvlk/image/imageResource/2020/04/17/1587126714469-931c9ef444f28fcc842b62ee2d1bc2ec.jpeg?tr=q-75,w-295"
               alt=""
             />
@@ -84,7 +84,7 @@ function RentcarPopularCar() {
           </div>
           <div className="relative cursor-pointer overflow-hidden rounded-lg border border-black m-1">
             <img
-              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110 hover:scale-110 w-full h-full"
+              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110 w-full h-full"
               src="https://ik.imagekit.io/tvlk/image/imageResource/2020/04/17/1587126717428-d908efa0b2cb7f4a43b287105990319a.jpeg?tr=q-75,w-295"
               alt=""
             />
@@ -94,7 +94,7 @@ function RentcarPopularCar() {
           </div>
           <div className="relative cursor-pointer overflow-hidden rounded-lg border border-black m-1">
             <img
-              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110 hover:scale-110"
+              className="blur-[2px] hover:blur-0 duration-200 hover:brightness-110"
               src="https://ik.imagekit.io/tvlk/image/imageResource/2020/04/17/1587126719458-b5084120bda8aaa4a08855cc8010ff47.jpeg?tr=q-75,w-295"
               alt=""
             />
