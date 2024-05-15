197307cab8c44d1dbd4a79395338b15fc9475ed9
NguyenLannnFE <nguyenhoanglan0311@gmail.com>
2024-05-15 12:17:30 +0700

    message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./Component/travelokecss/traveloke.scss";
-import HeaderAirplaneComponent from "./Component/traveloke-airplane/headerAirplane";
-import FooterAirplaneComponent from "./Component/traveloke-airplane/footerAirplane";
+import HeaderAirplaneComponent from "./Component/traveloke-airplane/header";
+import FooterAirplaneComponent from "./Component/traveloke-airplane/footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainAipPlane from "./Component/traveloke-airplane/MainAipPlane";
 import MainRentCarComponent from "./Component/traveloke-rentcar/MainRentCarComponent";
